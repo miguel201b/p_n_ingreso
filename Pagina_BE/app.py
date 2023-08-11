7e719c52ecc2d4d506b0aaf8417374ad035e9fc0
miguel201b <miguelcniyc@gmail.com>
2023-08-10 22:04:19 -0600

    cambios en el calendario

--- Pagina_BE/app.py
+++ Pagina_BE/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo como andamios?"
+    return "Hola mundo ¿como andamios?"
 
 if __name__ == '__main__':
     app.run(debug = True)
