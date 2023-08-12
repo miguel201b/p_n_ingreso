cfef72ed40166b15e1f25970f42d330c576512b5
miguel201b <miguelcniyc@gmail.com>
2023-08-12 17:54:36 -0600

    registro

--- pagina_backend/app.py
+++ pagina_backend/app.py
@@ -1,11 +1,69 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash,Response,jsonify
+import csv
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from io import StringIO
+import os
+from unidecode import unidecode
+
+def formato_apellido(input_str):
+    return unidecode(input_str).lower()
+
+def assign_group(apellido):
+    if 'a' <= apellido[0] <= 'f':
+        time_slot = "9"
+    elif 'g' <= apellido[0] <= 'm':
+        time_slot = "11"
+    else:
+        time_slot = "13"
+    group = Groups.query.filter_by(time_slot=time_slot).filter(Groups.current_students < Groups.max_students).first()
+    if not group:
+        raise Exception(f"No hay grupos disponibles para el horario {time_slot}")
+    return group
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "random_string"
-
+ 
+#Modelo de la base de datos
 db = SQLAlchemy(app)
+ 
+class Students(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key=True)
+    apellido = db.Column(db.String(100),nullable = False)
+    cuenta = db.Column(db.String(10), nullable = False)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=True)
+    group = db.relationship('Groups', backref=db.backref('students', lazy=True))
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+class Groups(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    max_students = db.Column(db.Integer, default=55)
+    current_students = db.Column(db.Integer, default=0)
+    time_slot = db.Column(db.String(10), nullable=False) # Esto puede ser "9", "11" o "13"
+
+    
+@app.route('/download_csv')
+def download_csv():
+    # Realizar la consulta
+    students = Students.query.all()
+
+    # Usa StringIO para simular un archivo en memoria
+    si = StringIO()
+    writer = csv.writer(si)
+
+    # Escribe los encabezados y datos en el "archivo"
+    writer.writerow(['ID', 'Apellido', 'Cuenta'])
+    for student in students:
+        writer.writerow([student.id, student.apellido, student.cuenta])
+
+    # Crea una respuesta con el contenido del "archivo"
+    response = Response(si.getvalue(), content_type='text/csv')
+    response.headers['Content-Disposition'] = 'attachment; filename=students.csv'
+
+    return response
 
 @app.route('/instructivo')
 def instructivo():
@@ -23,29 +81,34 @@ def horario():
 def idex1():
     return render_template('index.html')
 
-    
-class Students(db.Model):
-    id = db.Column('student_id', db.Integer, primary_key=True)
-    apellido = db.Column(db.String(100))
-    cuenta = db.Column(db.String(10))
-
 @app.route('/')
 def index():
     return render_template('Horario.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/registrados')
+def registrados():
+    title = 'Alumnos registrados'
+    students = Students.query.all() 
+    return render_template('registrados.html', title=title,students=students)
+
+@app.route('/horario', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         apellido = request.form['apellido']
+        apellido = formato_apellido(apellido)
         cuenta = request.form['cuenta']
-        
-        # Aquí deberías verificar la disponibilidad del grupo y asignar al estudiante a un grupo.
-        # Por simplicidad, sólo estamos registrando al estudiante.
 
-        student = Students(apellido=apellido, cuenta=cuenta)
-        db.session.add(student)
-        db.session.commit()
-        flash('Estudiante registrado con éxito!')
+        group = assign_group(apellido)
+        group.current_students += 1
+
+        try:
+            student = Students(apellido=apellido, cuenta=cuenta,group = group)
+            db.session.add(student)
+            db.session.commit()
+            return jsonify({"success": True, "message": "Estudiante registrado con éxito!"})
+        except Exception as e:
+            print(e)  
+            return jsonify({"success": False, "message": "Error registrando al estudiante. Por favor, inténtalo de nuevo."})
         
     return redirect(url_for('index'))
 
--- pagina_backend/static/js/datos.js
+++ pagina_backend/static/js/datos.js
@@ -1,11 +1,35 @@
-function obtenerPrimeraLetra(apellido) {
-    return apellido.charAt(0).toUpperCase();
-}
+document.querySelector("#boton").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-document.querySelector("#boton").addEventListener("click", function() {
-        var apellido = document.getElementById("apellido").value;
-        var primeraLetra = obtenerPrimeraLetra(apellido);
-        var cuenta = document.getElementById("ncuenta").value;
+    var apellido = document.getElementById("apellido").value;
+    var cuenta = document.getElementById("ncuenta").value;
 
-        alert("Apellido: " + primeraLetra + "\nNúmero de cuenta: " + cuenta);
+    // Crear un objeto FormData para enviar los datos al servidor
+    var formData = new FormData();
+    formData.append("apellido", apellido);
+    formData.append("cuenta", cuenta);
+
+    // Realizar la solicitud POST al servidor
+    fetch("/horario", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Aquí puedes manejar la respuesta del servidor
+        if (data.success) {
+            showAlert(data.message);
+            window.location.href = '/'; // Redirecciona al inicio (o donde quieras) después de mostrar el mensaje
+        } else {
+            showAlert(data.message);
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        showAlert("Hubo un error al enviar los datos. Por favor, inténtalo de nuevo.");
     });
+});
+
+function showAlert(message) {
+    alert(message);
+}
